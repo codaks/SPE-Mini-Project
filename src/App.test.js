2b08dd36efcc9f50affcfa99c018649c13f82ab6
Chitransh Kulshrestha <48233397+codaks@users.noreply.github.com>
2024-02-14 17:04:16 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,13 +3,13 @@ import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
 test('renders calculator with initial display value', () => {
-  const { getByText } = render(<Calculator />);
+  const { getByText } = render(<App />);
   const displayElement = getByText(/0/i);
   expect(displayElement).toBeInTheDocument();
 });
 
 test('clicking numbers updates the display', () => {
-  const { getByText } = render(<Calculator />);
+  const { getByText } = render(<App />);
   fireEvent.click(getByText('1'));
   fireEvent.click(getByText('2'));
   fireEvent.click(getByText('3'));
@@ -18,7 +18,7 @@ test('clicking numbers updates the display', () => {
 });
 
 test('performing addition operation', () => {
-  const { getByText } = render(<Calculator />);
+  const { getByText } = render(<App />);
   fireEvent.click(getByText('1'));
   fireEvent.click(getByText('+'));
   fireEvent.click(getByText('2'));
